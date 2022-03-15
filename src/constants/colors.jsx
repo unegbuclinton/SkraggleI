a2fc841182bda6409da226c587dbae708f11928a
Agiamah Precious <agiamah@biggorillaapps.com>
2022-03-15 11:39:10 +0100

    chore - initial commit && setup

---
+++ src/constants/colors.jsx
@@ -0,0 +1,16 @@
+export const COLORS = {
+  white: '#FFFFFF',
+  black: '#000000',
+  deepPurple: '#1E003E',
+  pink: '#FF576B',
+  torquoise: '#E6EFF1',
+  'porcelain-white': '#E2E6FF',
+  'bg-light-blue': '#EEF3FF',
+  'header-grey': '#1F2847',
+  'grey-100': '#8C8C8C',
+  'grey-200': '#626262',
+  'grey-300': '#5E5E5E',
+  'grey-400': '#585858',
+  'garage-mix-grey': '#E9ECEF',
+  'snooze-grey': 'rgba(19, 41, 61, 0.6)',
+};
---
+++ src/constants/font-spec.jsx
@@ -0,0 +1,13 @@
+export const FONTSIZES = {
+  xsmall: '1.2rem',
+  small: '1.4rem',
+  base: '1.6rem',
+  lg: '1.8rem',
+  xlarge: '2rem'
+};
+
+export const FONTWEIGHTS = {
+  normal: 400,
+  medium: 500,
+  bold: 700,
+};
