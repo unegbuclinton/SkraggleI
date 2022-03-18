7e6d229c9f08b138a5560e5f39b76993a89d483a
fateh <fateh.allam@gmail.com>
2022-03-18 21:10:02 +0500

    formik and validation

--- src/components/molecules/LogIn/LogIn.jsx
+++ src/components/molecules/LogIn/LogIn.jsx
@@ -1,28 +1,68 @@
 import React from "react";
 
+import { useFormik } from "formik";
+import { loginSchema } from "Validation/Schema";
+
 import Input from "components/atoms/Input/Input";
 import AuthLayout from "components/layouts/AuthLayout";
 import Button from "components/atoms/Button/Button";
-import { LoginWrapper, FormWrapper, LoginLink } from "./styles";
+import Card from "components/atoms/Card/Card";
+
+import { FormWrapper, LoginLink, ErrorMsg } from "./styles";
 
 const LogIn = () => {
+  const formik = useFormik({
+    initialValues: {
+      password: "",
+    },
+    validationSchema: loginSchema,
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <AuthLayout>
-      <LoginWrapper>
-        <FormWrapper>
+        <FormWrapper onSubmit={formik.handleSubmit}>
+          <Card className="login-card">
           <h1 className="login-heading">Log In</h1>
-          <Input type="text" placeholder="Email"></Input>
-          <Input type="password" placeholder="Password"></Input>
+          <Input
+            type="text"
+            placeholder="Email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <ErrorMsg>{formik.errors.email}</ErrorMsg>
+          ) : null}
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <ErrorMsg>{formik.errors.password}</ErrorMsg>
+          ) : null}
           <LoginLink className="link__forgotpassword" to={"/forgot-password"}>
             Forgot Password?
           </LoginLink>
           <Button auth>LOG IN</Button>
           <label>
             Don’t have any account?
-            <LoginLink className="link__signup" to="/sign-up"> Sign Up</LoginLink>
+            <LoginLink className="link__signup" to="/sign-up">
+              {" "}
+              Sign Up
+            </LoginLink>
           </label>
+          </Card> 
         </FormWrapper>
-      </LoginWrapper>
     </AuthLayout>
   );
 };
--- src/components/molecules/LogIn/styles.jsx
+++ src/components/molecules/LogIn/styles.jsx
@@ -3,9 +3,15 @@ import { FONTSIZES, FONTWEIGHTS } from "constants/font-spec";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const LoginWrapper = styled.div`
-  padding-left: 4.5rem;
-  padding-right: 4.5rem;
+
+export const FormWrapper = styled.form`
+  margin-top: 11.734rem;
+  .login-card {
+    padding-left: 4.5rem;
+    padding-right: 4.5rem;
+    padding-top: 3.439rem;
+    padding-bottom: 4rem;
+  }
 
   .login-heading {
     display: flex;
@@ -29,14 +35,12 @@ export const LoginWrapper = styled.div`
   }
 `;
 
-export const FormWrapper = styled.form``;
-
 export const LoginLink = styled(Link)`
   &.link {
     &__forgotpassword {
       font-weight: ${FONTWEIGHTS.medium};
       font-size: ${FONTSIZES.small};
-      line-height: 1.6REM;
+      line-height: 1.6rem;
       text-decoration: none;
       display: flex;
       justify-content: right;
@@ -51,3 +55,10 @@ export const LoginLink = styled(Link)`
   }
 `;
 
+export const ErrorMsg = styled.p`
+  color: red;
+  margin-bottom: 1rem;
+  font-size: ${FONTSIZES.small};
+  font-weight: ${FONTWEIGHTS.medium};
+  margin-top: -0.5rem;
+`;
