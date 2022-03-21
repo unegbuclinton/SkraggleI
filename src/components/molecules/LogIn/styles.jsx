e11486ae290373337e5ea04d4287299563bd99e4
fateh <fateh.allam@gmail.com>
2022-03-21 13:48:36 +0500

    minor fixes

--- src/components/molecules/LogIn/styles.jsx
+++ src/components/molecules/LogIn/styles.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const FormWrapper = styled.form`
-  margin-top: 11.734rem;
+  padding-top: 11.734rem;
   .login-card {
     padding-left: 4.5rem;
     padding-right: 4.5rem;
@@ -44,22 +44,21 @@ export const FormWrapper = styled.form`
 `;
 
 export const LoginLink = styled(Link)`
-  &.link {
-    &__forgotpassword {
-      font-weight: ${FONTWEIGHTS.medium};
-      font-size: ${FONTSIZES.small};
-      line-height: 1.6rem;
-      text-decoration: none;
-      display: flex;
-      justify-content: right;
-      margin-top: 0.8rem;
-      margin-bottom: 5.6rem;
-      color: #ff576b;
-    }
+  font-weight: ${FONTWEIGHTS.medium};
+  font-size: ${FONTSIZES.small};
+  line-height: 1.6rem;
+  text-decoration: none;
+  display: flex;
 
-    &__signup {
-      color: #1e003e;
-    }
+  &.forgot-password {   
+    justify-content: right;
+    margin-top: 0.8rem;
+    margin-bottom: 5.6rem;
+    color: #ff576b;
+  }
+
+  &.signup {
+    color: #1e003e;
   }
 `;
 
