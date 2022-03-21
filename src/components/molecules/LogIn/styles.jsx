e17979ed3d8ed26fe57041b92d6d0b403ffa7f75
fateh <fateh.allam@gmail.com>
2022-03-21 14:35:25 +0500

    minor fixes

--- src/components/molecules/LogIn/styles.jsx
+++ src/components/molecules/LogIn/styles.jsx
@@ -10,27 +10,29 @@ export const FormWrapper = styled.form`
     padding-right: 4.5rem;
     padding-top: 3.439rem;
     padding-bottom: 4rem;
-  }
 
-  .login-heading {
-    display: flex;
-    justify-content: center;
-    font-weight: ${FONTWEIGHTS.bold};
-    font-size: ${FONTSIZES.xlarge};
-    line-height: 2.3rem;
-    color: ${COLORS["blue-black"]};
-    padding-top: 3.44rem;
-    padding-bottom: 3.86rem;
-  }
+    &__heading {
+      display: flex;
+      justify-content: center;
+      font-weight: ${FONTWEIGHTS.bold};
+      font-size: ${FONTSIZES.xlarge};
+      line-height: 2.3rem;
+      color: ${COLORS["blue-black"]};
+      padding-top: 3.44rem;
+      padding-bottom: 3.86rem;
+    }
 
-  label {
-    font-weight: ${FONTWEIGHTS.medium};
-    font-size: ${FONTSIZES.small};
-    line-height: 1.6rem;
-    color: ${COLORS["gray-500"]};
-    display: flex;
-    justify-content: center;
-    margin-bottom: 4rem;
+    &__signup-link {
+      display: flex;
+      gap: 0.8rem;
+      font-weight: ${FONTWEIGHTS.medium};
+      font-size: ${FONTSIZES.small};
+      line-height: 1.6rem;
+      color: ${COLORS["gray-500"]};
+      display: flex;
+      justify-content: center;
+      margin-bottom: 4rem;
+    }
   }
 
   .login-button {
@@ -50,7 +52,7 @@ export const LoginLink = styled(Link)`
   text-decoration: none;
   display: flex;
 
-  &.forgot-password {   
+  &.forgot-password {
     justify-content: right;
     margin-top: 0.8rem;
     margin-bottom: 5.6rem;
