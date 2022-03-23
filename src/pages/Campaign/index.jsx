6a4f33aa20380fa71c42ab75d7b24fb9c79943ab
fateh <fateh.allam@gmail.com>
2022-03-23 20:19:29 +0500

    Tabs added

--- src/pages/Campaign/index.jsx
+++ src/pages/Campaign/index.jsx
@@ -2,9 +2,10 @@ import { React, useState } from "react";
 import { Link } from "react-router-dom";
 
 import DashboardLayout from "components/layouts/DashboardLayout";
-import { TabLinksWrapper, CampaignBody } from "./styles";
+import { CampaignBody } from "./styles";
 import ArchiveTable from "components/molecules/Campaign/Tables/ArchiveTable";
 import CampaignTable from "components/molecules/Campaign/Tables/CampaignTable";
+import Tabs from "components/atoms/Tabs";
 
 const Campaign = () => {
   const links = [
@@ -22,32 +23,25 @@ const Campaign = () => {
 
   const [active, setActive] = useState(false);
 
-  const fireActive = (index) => {
-    setActive(index);
-    console.log(index);
+  const handleActive = (e) => {
+    setActive(Number(e.target.id));
   };
+
   return (
     <DashboardLayout>
-      <TabLinksWrapper>
-      {links.map((link, index) => (
-        <Link
-          onClick={() => {
-            fireActive(index);
-          }}
-          className={index === active ? "link__item__active" : "each__link"}
-          to={link.path}
+        <Tabs
+          data={links}
+          active={active}
+          onClick={handleActive}
+          itemActive="link__item__active"
+          normalLink="each__link"
         >
-          {link.label}
-          
-        </Link>
-      ))}
-    </TabLinksWrapper>
-      
-      <CampaignBody>
-        {active === 0 && <CampaignTable/>}
-        {active === 1 && <ArchiveTable/>}
-      </CampaignBody>
+        </Tabs>
 
+      <CampaignBody>
+        {active === 1 && <CampaignTable />}
+        {active === 2 && <ArchiveTable />}
+      </CampaignBody>
     </DashboardLayout>
   );
 };
