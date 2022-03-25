128e88b995000a8effb28b411663766ca06ee725
Agiamah Precious <agiamah@biggorillaapps.com>
2022-03-25 10:59:28 +0100

    fix-remove unused variables

--- src/pages/modals/SetRevenueModal/index.jsx
+++ src/pages/modals/SetRevenueModal/index.jsx
@@ -1,13 +1,13 @@
-import Modal from "components/layouts/Modal";
-import SetRevenueModalContent from "components/molecules/SetRevenue";
-import React from "react";
+import Modal from 'components/layouts/Modal';
+import SetRevenueModalContent from 'components/molecules/SetRevenue';
+import React from 'react';
 
-function SetRevenueModal({onClose, isShown}) {
+function SetRevenueModal({ onClose, isShown }) {
   return (
     <Modal header="Set Revenue Goals" isShown={isShown}>
-      <SetRevenueModalContent  onClose={onClose}/> 
+      <SetRevenueModalContent onClose={onClose} />
     </Modal>
-  ); 
+  );
 }
 
 export default SetRevenueModal;
