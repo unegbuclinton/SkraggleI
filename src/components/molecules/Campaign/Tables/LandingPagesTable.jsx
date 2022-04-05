7670d9d21d8ef0e2834435442c3030f77e496a27
Agiamah Precious <agiamah@biggorillaapps.com>
2022-04-05 17:55:49 +0100

    chore - remove unused variables

--- src/components/molecules/Campaign/Tables/LandingPagesTable.jsx
+++ src/components/molecules/Campaign/Tables/LandingPagesTable.jsx
@@ -1,51 +1,46 @@
-import { React, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-import CustomDropdown from "components/atoms/CustomDropdown/CustomDropdown";
-import SearchBar from "components/atoms/SearchBar/SearchBar";
-import Table from "components/layouts/Table";
-import Button from "components/atoms/Button/Button";
-import Modal from "components/layouts/Modal";
-import Pagination from "components/molecules/Pagination";
-import { DPPlusIcon } from "icons";
-
-import datas from "utilities/filterData";
-
-import { landingPagesData } from "utilities/campaigndata";
-
-import { TableWrapper, TableHeaderWrapper, Box, ContainerBody } from "./styles";
+import Button from 'components/atoms/Button/Button';
+import CustomDropdown from 'components/atoms/CustomDropdown/CustomDropdown';
+import SearchBar from 'components/atoms/SearchBar/SearchBar';
+import Table from 'components/layouts/Table';
+import Pagination from 'components/molecules/Pagination';
+import { DPPlusIcon } from 'icons';
+import { React, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { landingPagesData } from 'utilities/campaigndata';
+import datas from 'utilities/filterData';
+import { Box, ContainerBody, TableHeaderWrapper, TableWrapper } from './styles';
 
 const LandingPagesTable = () => {
   const columns = [
     {
-      name: " ",
+      name: ' ',
       cell: () => <Box type="checkbox"></Box>,
       ignoreRowClick: false,
-      width: "5rem",
+      width: '5rem',
     },
     {
-      name: "ID",
+      name: 'ID',
       selector: (row) => row.uid,
-      width: "20rem",
+      width: '20rem',
     },
 
     {
-      name: "NAME",
+      name: 'NAME',
       selector: (row) => row.name,
-      width: "20rem",
+      width: '20rem',
     },
     {
-      name: "CAMPAIGN",
+      name: 'CAMPAIGN',
       selector: (row) => row.campaign,
-      width: "20rem",
+      width: '20rem',
     },
     {
-      name: "TYPE",
+      name: 'TYPE',
       selector: (row) => row.type,
-      width: "20rem",
+      width: '20rem',
     },
     {
-      name: "ACTION",
+      name: 'ACTION',
       selector: (row) => row.action,
       cell: (col) => (
         <span>
@@ -54,10 +49,10 @@ const LandingPagesTable = () => {
           <Button className="table-button__remove">Active</Button>
         </span>
       ),
-      width: "35rem",
+      width: '35rem',
     },
     {
-      name: "ADVANCE",
+      name: 'ADVANCE',
       selector: (row) => row.advance,
       cell: (col) => (
         <span className="table-button__span">
@@ -91,38 +86,43 @@ const LandingPagesTable = () => {
 
   let navigate = useNavigate();
   const handleButtonClick = () => {
-    navigate("/landing-page");
+    navigate('/landing-page');
   };
 
   return (
     <div>
-  <ContainerBody>
-    <TableWrapper>
-      <TableHeaderWrapper className="table-header">
-        <div className="table-header__left">
-          <h1>88 Landing Pages</h1>
-        </div>
+      <ContainerBody>
+        <TableWrapper>
+          <TableHeaderWrapper className="table-header">
+            <div className="table-header__left">
+              <h1>88 Landing Pages</h1>
+            </div>
 
-        <div className="table-header__right">
-          <CustomDropdown className="dropdown-filter" data={datas} />
-          <SearchBar className="search-icon" />
-          <Button
-            className="campaign-button"
-            onClick={() => handleButtonClick()}
-          >
-            <DPPlusIcon className="plus-icon" />
-            Create New
-          </Button>
-        </div>
-      </TableHeaderWrapper>
-      <Table columns={columns} data={currentList} onRowClicked={onRowClicked} />
-    </TableWrapper>
-    </ContainerBody>
-    <Pagination
-    currentPage={currentPage}
-    itemsPerPage={itemsPerPage}
-    data={landingPagesData}
-    setCurrentPage={setCurrentPage}/>
+            <div className="table-header__right">
+              <CustomDropdown className="dropdown-filter" data={datas} />
+              <SearchBar className="search-icon" />
+              <Button
+                className="campaign-button"
+                onClick={() => handleButtonClick()}
+              >
+                <DPPlusIcon className="plus-icon" />
+                Create New
+              </Button>
+            </div>
+          </TableHeaderWrapper>
+          <Table
+            columns={columns}
+            data={currentList}
+            onRowClicked={onRowClicked}
+          />
+        </TableWrapper>
+      </ContainerBody>
+      <Pagination
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+        data={landingPagesData}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 };
