b696a1d539c254e0b16c0acc011010c8ba3dc6d3
Agiamah Precious <agiamah@biggorillaapps.com>
2022-04-19 17:16:53 +0100

    fix - Change radio label size to fix text

--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -1,19 +1,31 @@
-import React from "react";
-import styled from "styled-components";
-import SubscriptionUpdate from "components/molecules/MailblastModalComponents/SubscriptionUpdate";
+import RadioGroup from 'components/atoms/RadioGroup';
+import SubscriptionUpdate from 'components/molecules/MailblastModalComponents/SubscriptionUpdate';
+import React from 'react';
+import styled from 'styled-components';
 const Test = () => {
   return (
     <Wrapper>
-      <SubscriptionUpdate />
+      <RadioGroup
+        radioData={[
+          { labelText: 'First', value: 1 },
+          {
+            labelText:
+              'Lorem ipsum, dolor sit amet consectetur',
+            value: 2,
+          },
+        ]}
+      />
     </Wrapper>
   );
 };
 
+
+
 export default Test;
 
 const Wrapper = styled.div`
-  .checkbox {
+  /* .checkbox {
     width: 2rem;
     height: 2rem;
-  }
+  } */
 `;
