dbb35ef63e87311738ac8c128f2b9e2dd0c66c64
agboola <agboolanathaniel@yahoo.com>
2022-05-06 15:18:50 +0100

    update folder name

--- src/routes/routePaths.jsx
+++ src/routes/routePaths.jsx
@@ -1,155 +1,155 @@
-import React from "react";
-import DashboardLayout from "components/layouts/DashboardLayout";
-import ForgotPassword from "pages/ForgotPassword";
-import Registration from "components/molecules/Registration";
-import ResendVerification from "components/molecules/resendVerification/ResendVerification";
-import ResetPassword from "components/molecules/ResetPassword";
-import SendVerification from "components/molecules/sendVerification/SendVerification";
-import Campaign from "pages/Campaign";
-import CampaignDetails from "pages/CampaignDetails";
-import Contacts from "pages/ContactsPage";
-import Profile from "pages/ContactsPage/Profile";
-import Dashboard from "pages/Dashboard";
-import Donations from "pages/Donations";
-import Events from "pages/Events";
-import P2P from "pages/P2P";
-import P2PDetails from "pages/P2PDetails";
-import MailBlast from "pages/MailBlast";
-import { Link, Outlet } from "react-router-dom";
-import MonthlyNewsteller from "pages/MailBlast/MonthlyNewsteller";
-import EventsDetails from "components/molecules/EventsDetails";
-import RegistrationPackage from "components/molecules/EventsDetails/RegistrationPackage";
-import Test from "pages/Test";
-import UnsubscribeWarning from "pages/MailBlast/UnsubscribeWarning";
-import LogIn from "pages/LogIn";
+import React from 'react';
+import DashboardLayout from 'components/layouts/DashboardLayout';
+import ForgotPassword from 'pages/ForgotPassword';
+import Registration from 'components/molecules/Registration';
+import ResendVerification from 'components/molecules/ResendVerification';
+import ResetPassword from 'components/molecules/ResetPassword';
+import SendVerification from 'components/molecules/SendVerification';
+import Campaign from 'pages/Campaign';
+import CampaignDetails from 'pages/CampaignDetails';
+import Contacts from 'pages/ContactsPage';
+import Profile from 'pages/ContactsPage/Profile';
+import Dashboard from 'pages/Dashboard';
+import Donations from 'pages/Donations';
+import Events from 'pages/Events';
+import P2P from 'pages/P2P';
+import P2PDetails from 'pages/P2PDetails';
+import MailBlast from 'pages/MailBlast';
+import { Link, Outlet } from 'react-router-dom';
+import MonthlyNewsteller from 'pages/MailBlast/MonthlyNewsteller';
+import EventsDetails from 'components/molecules/EventsDetails';
+import RegistrationPackage from 'components/molecules/EventsDetails/RegistrationPackage';
+import Test from 'pages/Test';
+import UnsubscribeWarning from 'pages/MailBlast/UnsubscribeWarning';
+import LogIn from 'pages/LogIn';
 
 export const privateRoutes = [
   {
-    path: "dashboard/*",
+    path: 'dashboard/*',
     element: <Outlet />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Dashboard />,
       },
       {
-        path: "smart",
+        path: 'smart',
         element: <Campaign />,
       },
       {
-        path: "contact",
+        path: 'contact',
         element: <SendVerification />,
       },
       {
-        path: "fundraising",
+        path: 'fundraising',
         element: <SendVerification />,
       },
     ],
   },
 
   {
-    path: "contacts/*",
+    path: 'contacts/*',
     element: <Outlet />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Contacts />,
       },
       {
-        path: "contact-profile",
+        path: 'contact-profile',
         element: <Profile />,
       },
     ],
   },
 
   {
-    path: "/donations",
+    path: '/donations',
     element: <Donations />,
   },
   {
-    path: "mail-blasts/*",
+    path: 'mail-blasts/*',
     element: <Outlet />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <MailBlast />,
       },
       {
-        path: ":id",
+        path: ':id',
         element: <MonthlyNewsteller />,
       },
       {
-        path: "subscription-update",
+        path: 'subscription-update',
         element: <UnsubscribeWarning />,
       },
     ],
   },
   {
-    path: "campaign/*",
+    path: 'campaign/*',
     element: <Outlet />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Campaign />,
       },
       {
-        path: ":id",
+        path: ':id',
         element: <CampaignDetails />,
       },
     ],
   },
   {
-    path: "events/*",
+    path: 'events/*',
     element: <Outlet />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Events />,
       },
       {
-        path: "events-details",
+        path: 'events-details',
         element: <EventsDetails />,
       },
       {
-        path: "registration-package",
+        path: 'registration-package',
         element: <RegistrationPackage />,
       },
     ],
   },
   {
-    path: "peer-to-peer/*",
+    path: 'peer-to-peer/*',
     element: <Outlet />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <P2P />,
       },
       {
-        path: ":id",
+        path: ':id',
         element: <P2PDetails />,
       },
     ],
   },
   {
-    path: "/forms",
+    path: '/forms',
     element: <DashboardLayout>Forms yet to be added</DashboardLayout>,
   },
 
   {
-    path: "/elements",
+    path: '/elements',
     element: <DashboardLayout>Elements yet to be added</DashboardLayout>,
   },
 
   {
-    path: "/landing-page",
+    path: '/landing-page',
     element: <DashboardLayout>Landing Page yet to be added</DashboardLayout>,
   },
   {
-    path: "/reports",
+    path: '/reports',
     element: <DashboardLayout>Reports yet to be added</DashboardLayout>,
   },
   {
-    path: "*",
+    path: '*',
     element: (
       <DashboardLayout>
         <h1>404</h1>
@@ -158,38 +158,38 @@ export const privateRoutes = [
   },
 
   {
-    path: "/test",
+    path: '/test',
     element: <Test />,
   },
 ];
 
 export const publicRoutes = [
   {
-    path: "/",
+    path: '/',
     element: <Link to="/login">Go to Login</Link>,
   },
   {
-    path: "/login",
+    path: '/login',
     element: <LogIn onClick />,
   },
   {
-    path: "/forgotpassword",
+    path: '/forgotpassword',
     element: <ForgotPassword />,
   },
   {
-    path: "/signup",
+    path: '/signup',
     element: <Registration />,
   },
   {
-    path: "/reset-password",
+    path: '/reset-password',
     element: <ResetPassword />,
   },
   {
-    path: "/resend-verification",
+    path: '/resend-verification',
     element: <ResendVerification />,
   },
   {
-    path: "/send-verification",
+    path: '/send-verification',
     element: <SendVerification />,
   },
 ];
