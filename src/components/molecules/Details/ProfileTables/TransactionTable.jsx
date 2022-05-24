0f2e03dd0913ab37c9dde84b2a2da28f55ca229e
Bear_bear <unegbuclinton5@gmail.com>
2022-05-24 21:46:34 +0100

    SB-246 : Add empty state for Donation sub-tab

--- src/components/molecules/Details/ProfileTables/TransactionTable.jsx
+++ src/components/molecules/Details/ProfileTables/TransactionTable.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-extra-boolean-cast */
 import Table from 'components/layouts/Table';
+import DonationSubTabEmptyState from 'components/molecules/EmptyState/ContactSubTabs/Donations';
 import Pagination from 'components/molecules/Pagination';
 import React, { useState } from 'react';
 import { columns, data } from 'utilities/transactionData';
@@ -14,21 +16,28 @@ function TransactionTable() {
   const indexFirstList = indexLasttList - itemsPerPage;
 
   const currentList = data.slice(indexFirstList, indexLasttList);
+
   return (
-    <ProfileLayOut heading="Transactions">
-      <TableWrapper>
-        <TableHeading>
-          <h2 className="heading">7 Transactions</h2>
-        </TableHeading>
-        <Table columns={columns} data={currentList} />
-      </TableWrapper>
-      <Pagination
-        currentPage={currentPage}
-        itemsPerPage={itemsPerPage}
-        data={data}
-        setCurrentPage={setCurrentPage}
-      />
-    </ProfileLayOut>
+    <>
+      {!!data.length ? (
+        <ProfileLayOut heading="Transactions">
+          <TableWrapper>
+            <TableHeading>
+              <h2 className="heading">7 Transactions</h2>
+            </TableHeading>
+            <Table columns={columns} data={currentList} />
+          </TableWrapper>
+          <Pagination
+            currentPage={currentPage}
+            itemsPerPage={itemsPerPage}
+            data={data}
+            setCurrentPage={setCurrentPage}
+          />
+        </ProfileLayOut>
+      ) : (
+        <DonationSubTabEmptyState />
+      )}
+    </>
   );
 }
 
