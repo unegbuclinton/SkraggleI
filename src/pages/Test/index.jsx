4ab274bf46e19e17201483d0c3e1161aa2f6c6dc
fateh <fateh.allam@gmail.com>
2022-06-06 22:42:01 +0500

    p2p slice fix

--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -1,15 +1,15 @@
 // import Spinner from 'components/atoms/Spinner/Spinner';
 import DashboardLayout from 'components/layouts/DashboardLayout';
 import FormCardLayout from 'components/layouts/FormCardLayout';
-import FormComponent from 'components/molecules/Forms';
+// import FormComponent from 'components/molecules/Forms';
 // import TableHeader from 'components/molecules/TableHeader/TableHeader';
 import React from 'react';
 
 function Test() {
-  const tabs = [
-    { title: 'Give Once', component: <FormComponent /> },
-    { title: 'Monthly', component: <FormComponent /> }
-  ];
+  // const tabs = [
+  //   { title: 'Give Once', component: <FormComponent /> },
+  //   { title: 'Monthly', component: <FormComponent /> }
+  // ];
   return (
     <DashboardLayout>
       <FormCardLayout></FormCardLayout>
