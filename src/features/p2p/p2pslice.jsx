0b3923c9798dcee5b5236af0bc4f3e7b87dde222
fateh <fateh.allam@gmail.com>
2022-06-06 22:17:09 +0500

    donation flow

--- src/features/p2p/p2pslice.jsx
+++ src/features/p2p/p2pslice.jsx
@@ -1,51 +1,26 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { addP2P, allP2P, p2pById } from 'api/p2p/overview';
-import { logoutUser } from 'features/auth/authSlice';
+import { getPeerToPeer, peerToPeer } from 'api/p2p/p2pFundRaiser';
 
 const initialState = {
-  p2pData: [],
-  isLoading: false,
-  eachP2p: []
+  p2pData: []
 };
 
-export const createP2P = createAsyncThunk('p2p/createContact', addP2P);
-export const viewP2P = createAsyncThunk('p2p/viewContact', allP2P);
-export const getEachP2p = createAsyncThunk('getEachP2p', p2pById);
+export const createP2p = createAsyncThunk('contact/createContact', peerToPeer);
 
-//search will go here
+export const fetchP2p = createAsyncThunk('contact/createContact', getPeerToPeer);
 
 export const p2pSlice = createSlice({
-  name: 'p2p',
+  name: 'peertopeer',
   initialState,
   reducers: {},
+
   extraReducers: {
-    //CREATE P2P
-
-    [createP2P.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [createP2P.fulfilled]: (state) => {
-      state.isLoading = false;
-    },
-
-    [createP2P.rejected]: (state) => {
-      state.isLoading = false;
-    },
-    //VIEW P2P
-    [viewP2P.fulfilled]: (state, action) => {
-      state.isLoading = false;
+    [createP2p.fulfilled]: (state, action) => {
       state.p2pData = action.payload;
     },
-    [viewP2P.rejected]: (state) => {
-      state.isLoading = false;
-    },
 
-    [getEachP2p.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.eachP2p = action.payload;
-    },
-    [logoutUser.fulfilled]: () => {
-      return initialState;
+    [fetchP2p.fulfilled]: (state, action) => {
+      state.p2pData = action.payload;
     }
   }
 });
