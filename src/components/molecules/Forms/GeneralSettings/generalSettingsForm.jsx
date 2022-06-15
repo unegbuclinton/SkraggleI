796e685a87437e3e44a9892987f54e55b77613f7
fateh <fateh.allam@gmail.com>
2022-06-15 15:41:43 +0500

    fix campaign layout

--- src/components/molecules/Forms/GeneralSettings/generalSettingsForm.jsx
+++ src/components/molecules/Forms/GeneralSettings/generalSettingsForm.jsx
@@ -113,12 +113,14 @@ export const GeneralSettingsFormWrapper = styled.form`
   width: 100%;
   overflow: hidden;
   height: 100%;
-  max-width: 53rem;
+  display: flex;
+  flex: 0.65;
 `;
 
 export const GeneralSettingsFormCard = styled(Card)`
   overflow: auto;
   height: 100%;
+  width: 100%;
   padding: 4rem 4.4rem 2.4rem 4.4rem;
 
   .modal-input {
@@ -127,7 +129,6 @@ export const GeneralSettingsFormCard = styled(Card)`
     background-color: transparent;
     border: 1px solid #9a9aa9;
     border-radius: 5px;
-    max-width: 44.2rem;
     width: 100%;
     color: ${COLORS.black};
   }
