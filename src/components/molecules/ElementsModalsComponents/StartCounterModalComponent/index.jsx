e4a815e6ecc521706461abfd36b684e8835603a0
agboola <agboolanathaniel@yahoo.com>
2022-06-24 17:47:17 +0100

    build element modal

--- src/components/molecules/ElementsModalsComponents/StartCounterModalComponent/index.jsx
+++ src/components/molecules/ElementsModalsComponents/StartCounterModalComponent/index.jsx
@@ -1,16 +1,20 @@
 import Button from 'components/atoms/Button/Button';
 import Card from 'components/atoms/Card';
 import Checkbox from 'components/atoms/CheckBox';
+import CopyField from 'components/atoms/CopyField';
 import SelectDropDown from 'components/atoms/GenericDropdown';
 import Input from 'components/atoms/Input/Input';
 import React from 'react';
 import {
+  CopyContainer,
+  CopyLabel,
   StartCounterContainer,
   StartCounterFieldWrapper,
   StartCounterFooter,
   StartCounterheader,
   StartCounterLabel,
-  StartCounterWrapper
+  StartCounterWrapper,
+  UrlLabel
 } from './styles';
 
 function StartCounterModalComponent() {
@@ -22,7 +26,7 @@ function StartCounterModalComponent() {
             Display an animated statistic of your campaign’s performance.
           </StartCounterheader>
           <StartCounterFieldWrapper>
-            <StartCounterLabel>Element Name</StartCounterLabel>
+            <StartCounterLabel className="element-label">Element Name</StartCounterLabel>
             <Input className="counter-input" placeholder="start-counter" />
           </StartCounterFieldWrapper>
         </StartCounterContainer>
@@ -34,30 +38,39 @@ function StartCounterModalComponent() {
         </StartCounterFieldWrapper>
         <StartCounterFieldWrapper>
           <StartCounterLabel> Campaign</StartCounterLabel>
-          <Input className="campaign-input" />
+          <Input className="campaign-input" placeholder="Select campaign" />
         </StartCounterFieldWrapper>
         <StartCounterFieldWrapper>
-          <StartCounterLabel>Campaign</StartCounterLabel>
-          <Input className="counter-input" />
+          <StartCounterLabel>Currency</StartCounterLabel>
+          <Input className="counter-input" placeholder="USD  |  US Dollar" />
         </StartCounterFieldWrapper>
 
         <StartCounterFieldWrapper>
           <StartCounterLabel>Start date</StartCounterLabel>
           <Checkbox label="Set start date" className="counter-checkbox" />
+          <UrlLabel>?</UrlLabel>
         </StartCounterFieldWrapper>
         <StartCounterFieldWrapper>
           <StartCounterLabel> End date</StartCounterLabel>
           <Checkbox label="Set end date" className="counter-checkbox" />
+          <UrlLabel className="set-date">?</UrlLabel>
         </StartCounterFieldWrapper>
         <StartCounterFieldWrapper>
-          <StartCounterLabel>Add amount</StartCounterLabel>
-          <Input className="amount-input" />
+          <StartCounterLabel className="add-amount">Add amount</StartCounterLabel>
+          <div>
+            <Input className="amount-input" placeholder="$0.00" />
+          </div>
+          <UrlLabel className="amount">?</UrlLabel>
         </StartCounterFieldWrapper>
         <StartCounterFieldWrapper>
-          <StartCounterLabel>Matching funds</StartCounterLabel>
+          <StartCounterLabel className="marchin-funds">Matching funds</StartCounterLabel>
           <Checkbox label="Set Multiplier" />
+          <UrlLabel className="multi">?</UrlLabel>
         </StartCounterFieldWrapper>
-
+        <CopyContainer>
+          <CopyLabel>HTML CODE</CopyLabel>
+          <CopyField grey />
+        </CopyContainer>
         <StartCounterFooter>
           <Button className="archive-btn" invert auth>
             Archive
