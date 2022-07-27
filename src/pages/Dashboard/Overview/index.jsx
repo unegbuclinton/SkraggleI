14dbeeb9f614a8b44bd9f08e39d888f0f4e3b2ec
agboola <agboolanathaniel@yahoo.com>
2022-07-27 13:48:18 +0100

    remove comented code

--- src/pages/Dashboard/Overview/index.jsx
+++ src/pages/Dashboard/Overview/index.jsx
@@ -1,48 +1,22 @@
 /* eslint-disable no-unused-vars */
-import { getActivities } from 'api/donation/fundraisingActivities';
 import Button from 'components/atoms/Button/Button';
 import Card from 'components/atoms/Card';
-import DropdownComponent from 'components/atoms/Dropdown';
-import Modal from 'components/layouts/Modal';
-import DateRange from 'components/molecules/DateRange';
-import DonationButton from 'components/molecules/iFrameComp/DonationButton';
-import DonationLevelDraft from 'components/molecules/iFrameComp/DonationLevel';
-import dayjs from 'dayjs';
-import { getAdminData } from 'features/auth/authSlice';
-import { getAllCampaigns } from 'features/campaign/campaignSlice';
-import {
-  allHouseHold,
-  getAllCompanies,
-  getAllTodos,
-  viewContact,
-  viewTags
-} from 'features/contact/contactSlice';
 import {
   donationHistory,
   fundActivities,
   getOneTimeTransaction,
-  getPledge,
   getRecurringTransaction,
   revenueGoal,
   revenueHistory
 } from 'features/donation/donationSlice';
-import { getAllElements } from 'features/elements/elementsSlice';
-import { getAllEvents } from 'features/events/eventSlice';
-import { getAllForm } from 'features/forms/formsSlice';
 import { allSubscriptionStatus, listAllMailBlast } from 'features/mailblast/mailBlastSlice';
-import { viewP2P } from 'features/p2p/p2pSlice';
-import { DPIconDateArrow, DPIconRangeIcon } from 'icons';
 import WidgetModal from 'pages/Dashboard/modals/WidgetModal';
-import Test from 'pages/Test';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import store from 'store';
-import { datas1 } from 'utilities/overviewData';
-import { DatePicker, OverviewLeft, OverviewRight, OverviewWrapper, WelcomeText } from './styles';
+import { OverviewLeft, OverviewRight, OverviewWrapper, WelcomeText } from './styles';
 
 function Overview() {
   const dispatch = useDispatch();
-  const [selected, setSelected] = useState('Filters');
   const [openWidget, setOpenWidget] = useState(false);
   const { token } = useSelector((state) => state.auth);
   const userData = token?.profile;
@@ -54,24 +28,6 @@ function Overview() {
   });
   const [datePick, setDatePick] = useState(false);
   const toogleDateRange = () => setDatePick((prev) => !prev);
-
-  // useEffect(() => {
-  //   dispatch(getAllCompanies());
-  //   dispatch(getAllEvents());
-  //   dispatch(getPledge());
-  //   dispatch(allHouseHold());
-  //   dispatch(viewTags());
-  //   dispatch(viewContact());
-  //   dispatch(viewP2P());
-  //   dispatch(getAllCampaigns());
-  //   dispatch(getAllForm());
-  //   dispatch(getAllElements());
-  //   dispatch(getAdminData());
-  //   dispatch(getAdminData());
-  // }, []);
-
-  // useEffect(() => {
-  // }, []);
 
   useEffect(() => {
     dispatch(listAllMailBlast());
@@ -88,32 +44,6 @@ function Overview() {
     <OverviewWrapper>
       <Card className="overview-card">
         <OverviewLeft>
-          {/* <p className="overview-heder__text">Overview</p>
-          <div className="overview-action__buttons">
-            <div className="range-picker" onClick={toogleDateRange}>
-              <DatePicker>
-                <DPIconRangeIcon />
-              </DatePicker>
-              <DatePicker>{dayjs(filterRange.startDate).format('MMM DD YYYY')}</DatePicker>
-              <DatePicker>
-                <DPIconDateArrow />
-              </DatePicker>
-              <DatePicker>{dayjs(filterRange.endDate).format('MMM DD YYYY')}</DatePicker>
-            </div>
-
-            <DropdownComponent selected={selected} setSelected={setSelected} data={datas1} />
-            <DropdownComponent selected={selected} setSelected={setSelected} data={datas1} />
-          </div>
-          {datePick && (
-            <DateRange
-              open={datePick}
-              toggle={toogleDateRange}
-              className="date-range-picker"
-              onChangeRange={(ranges) => {
-                setFilterRange(ranges);
-              }}
-            />
-          )} */}
           <WelcomeText>{`Welcome ${userName}`}</WelcomeText>
         </OverviewLeft>
         <OverviewRight>
