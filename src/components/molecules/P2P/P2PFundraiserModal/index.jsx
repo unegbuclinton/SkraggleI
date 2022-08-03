eabd2a82007549fd7dccb65f299a5f4605330798
fateh <fateh.allam@gmail.com>
2022-08-03 15:36:10 +0500

    p2p ui fixes

---
+++ src/components/molecules/P2P/P2PFundraiserModal/index.jsx
@@ -0,0 +1,17 @@
+import Modal from 'components/layouts/Modal';
+import React from 'react';
+import P2PFundraiserModalComponent from '../P2PFundraiserModalComponent';
+
+function P2PFundraiserModal({ onClose, isShown }) {
+  return (
+    <Modal
+      header="Create P2P Fundraiser"
+      //   onClose={() => setShowFirstModal(false)}
+      isShown={isShown}
+      hide={onClose}>
+      <P2PFundraiserModalComponent />
+    </Modal>
+  );
+}
+
+export default P2PFundraiserModal;
